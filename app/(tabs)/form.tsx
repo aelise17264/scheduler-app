063d14cba18486d07afc8e727ec601706576570d
annabeXeX <annab@exex.ai>
2025-09-21 14:38:31 -0600

    sending form values to user's email app

--- app/(tabs)/form.tsx
+++ app/(tabs)/form.tsx
@@ -7,6 +7,8 @@ import {
   StyleSheet,
   TextInput,
   Platform,
+  Linking,
+  Alert,
 } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
@@ -40,11 +42,37 @@ const validationSchema = Yup.object().shape({
 function EmailForm() {
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showTimePicker, setShowTimePicker] = useState(false);
+
+  const showFormAlert = () => {
+    Alert.alert(
+      "Missing Info",
+      "Looks like you missed something on this form. Please fill out every section before clicking Submit.",
+      [
+        {
+          text: "OK",
+        },
+      ],
+      {
+        cancelable: true,
+      },
+    );
+  };
   return (
     <>
       <AppHeader />
       <Formik
-        onSubmit={(values: any) => alert(JSON.stringify(values, null, 2))}
+        onSubmit={(values: any) => {
+          alert(JSON.stringify(values, null, 2));
+
+          const subject = "New Notary Appointment Request";
+          const body = `
+          Name: ${values.name}
+          Email: ${values.email}
+          Appointment: ${values.appointment.toLocaleString()}
+          `;
+          const email = `mailto:abrander.notary@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+          Linking.openURL(email);
+        }}
         initialValues={{ name: "", email: "", appointment: new Date() }}
         validationSchema={validationSchema}
       >
@@ -66,18 +94,12 @@ function EmailForm() {
                 placeholder="Your Name"
                 autoCapitalize="words"
               />
-              {/* {touched.name && errors.name && (
-                <Text style={styles.error}>{errors.name}</Text>
-              )} */}
               <TextInput
                 onChangeText={handleChange("email")}
                 onBlur={handleBlur("email")}
                 value={values.email}
                 placeholder="Your Email"
               />
-
-              {/* <Button title="Request Date" onPress={() => setOpen(true)} /> */}
-
               <Button
                 title="Pick a Date"
                 onPress={() => setShowDatePicker(true)}
@@ -89,7 +111,6 @@ function EmailForm() {
                   display={Platform.OS === "ios" ? "spinner" : "default"}
                   onChange={(event: DateTimePickerEvent, date?: Date) => {
                     if (event.type === "set" && date) {
-                      // update only the date part, keep time
                       const updated = new Date(values.appointment);
                       updated.setFullYear(
                         date.getFullYear(),
@@ -115,37 +136,29 @@ function EmailForm() {
                   display={Platform.OS === "ios" ? "spinner" : "default"}
                   onChange={(event: DateTimePickerEvent, date?: Date) => {
                     if (event.type === "set" && date) {
-                      // update only the time part, keep date
                       const updated = new Date(values.appointment);
-                      updated.setHours(date.getHours(), date.getMinutes());
+                      updated.setHours(
+                        date.getHours(),
+                        date.getMinutes(),
+                      );
                       setFieldValue("appointment", updated);
                     }
                     if (Platform.OS === "android") setShowTimePicker(false);
                   }}
                 />
               )}
-              {/* 
-              {open && (
-                  <DateTimePicker
-              value={values.appointment}
-              mode="time"
-              display={Platform.OS === "ios" ? "spinner" : "default"}
-              onChange={(event: DateTimePickerEvent, date?: Date) => {
-                if (event.type === "set" && date) {
-                  // update only the time part, keep date
-                  const updated = new Date(values.appointment);
-                  updated.setHours(date.getHours(), date.getMinutes());
-                  setFieldValue("appointment", updated);
-                }
-                if (Platform.OS === "android") setOpen(false);
-              }}
-            />
-              )} */}
-
-              {/* <AppointmentPicker /> */}
             </View>
             <View style={styles.buttonContainer}>
-              <Button onPress={() => handleSubmit} title="Submit" />
+              <Button
+                onPress={() => {
+                  if (errors.email || errors.name || errors.appointment) {
+                    showFormAlert();
+                  } else {
+                    handleSubmit();
+                  }
+                }}
+                title="Submit"
+              />
               <Text style={{ fontSize: 20 }}>
                 {JSON.stringify(values, null, 2)}
               </Text>
